92eda787b4c3e5cb0cd0a83e59657421dff2b4a0
jelizasilatei <silateijc90@gmail.com>
2023-03-18 21:09:18 +0300

    First class functions

---
+++ index.js
@@ -0,0 +1,17 @@
+function receivesAFunction(callback) {
+    callback();
+  }
+
+      function returnsANamedFunction() {
+        return function namedFunction() {
+          // code
+        }
+      }
+
+        function returnsAnAnonymousFunction() {
+        return function() {
+          // code
+        };
+      }
+      
+     
